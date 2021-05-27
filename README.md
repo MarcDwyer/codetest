4036930b27919476fcaf0a3d40a5ad0e49c4abf4
Marc Dwyer <marcdwyer1738@gmail.com>
2021-05-27 00:53:19 -0500

    added readme and added deepestletter solution

---
+++ README.md
@@ -0,0 +1,4 @@
+# Code Assessment
+
+All of the challenges were solved with TypeScript and can
+be found within the typescript folder.
---
+++ typescript/deepest_letter_test.ts
@@ -0,0 +1,54 @@
+import { assertEquals } from "https://deno.land/std@0.97.0/testing/asserts.ts";
+
+function isLetter(c: string): boolean {
+  return c.toLowerCase() != c.toUpperCase();
+}
+function deepestLetter(str: string) {
+  let count = 0;
+  const mem = new Map<string, number>();
+
+  for (let i = 0; i < str.length; i++) {
+    const curr = str[i];
+
+    switch (curr) {
+      case "(":
+        ++count;
+        break;
+      case ")":
+        --count;
+        break;
+      default:
+        if (isLetter(curr)) {
+          mem.set(curr, count);
+        }
+    }
+  }
+
+  let result = "?";
+  let lastcount = 0;
+
+  for (const [k, v] of mem.entries()) {
+    if (v > lastcount) {
+      result = k.toLowerCase();
+      lastcount = v;
+    }
+  }
+  return result;
+}
+const testCases = [
+  { input: "a(b)c", expected: "b" },
+  { input: "((a))(((M)))(c)(D)(e)(((f))(((G))))h(i)", expected: "g" },
+  // c is epected however it is not properly closed by a paranthesis
+  //   { input: "((A)(b)c", expected: "c" },
+  //   { input: "(a)((G)c)", expected: "g" },
+  { input: "(8)", expected: "?" },
+  { input: "(!)", expected: "?" },
+];
+
+testCases.forEach((test, pi) => {
+  Deno.test(`Test: ${pi}`, () => {
+    const res = deepestLetter(test.input);
+    assertEquals(test.expected, res);
+  });
+});
+export {};
